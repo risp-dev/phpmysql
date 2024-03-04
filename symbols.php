fc2b15a703e5f43fb3476847dc64992d7bd7641d
Rimas <119005269+risp-dev@users.noreply.github.com>
2024-03-04 20:15:19 +0200

    index explained

--- symbols.php
+++ symbols.php
@@ -2,10 +2,25 @@
 include __DIR__ . '/includes/header.html.php';
 $text = 'Home sweet home';
 ?>
-
+<h1>Symbols</h1>
 <table>
     <tr>
-    <td>Tekstas: <?= $text ?></td>
+    <td>Tekxt: <?= $text ?></td>
+    <h3>Index 0: 'H'
+Index 1: 'o',
+Index 2: 'm',
+Index 3: 'e',<br>
+Index 4: ' ',<br>
+Index 5: 's',
+Index 6: 'w',
+Index 7: 'e',
+Index 8: 'e',
+Index 9: 't',<br>
+Index 10: ' ',<br>
+Index 11: 'h',
+Index 12: 'o',
+Index 13: 'm',
+Index 14: 'e',<br></h3>
 </tr>
 <tr>
     <td>First match (case-sensitive): <?= strpos($text, 'ho') ?></td>
