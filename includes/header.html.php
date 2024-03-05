8625e6b98d1ed9caeb6b99d27e80bba47b18a9a5
Rimas <rimas.spakauskas@outlook.com>
2024-03-05 14:51:01 +0200

    nav update

--- includes/header.html.php
+++ includes/header.html.php
@@ -5,14 +5,14 @@
  <link rel="stylesheet" href="css/style.css" />
  </head>
  <body>
- <h1>Saldi parduotuvė</h1>
+ <h1>Sweet sweet super sweet</h1>
  <nav>
  <a href="index.php">Array</a> |
  <a href="arrayTwo.php">Array 2</a> |
  <a href="arrayThree.php">Array 3</a> |
- <a href="arrayFour">Array 4</a> |
+ <a href="arrayFour.php">Array 4</a> |
  <a href="numbers.php">Numbers</a> |
- <a href="symbols.php">Simboliai</a> |
+ <a href="symbols.php">Symbols</a> |
  <a href="reg-ex-functions.php">Regular expression functions</a> |
  <a href="function-one.php">F-1</a> |
  <a href="function-two.php">F-2</a> |
