6d91f58c07decc896e2609beafd8b1d7b816c67f
Rimas <rimas.spakauskas@outlook.com>
2024-03-14 16:52:45 +0200

    password validate

---
+++ password-check.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types =1);
+include __DIR__ . '/includes/header.html.php';
+
+$password = '';
+$message = '';
+
+function is_password(string $password): bool {
+ if(
+    mb_strlen($password) >= 5 
+    and preg_match('/[A-Z]/', $password)
+    and preg_match('/[a-z]/', $password)
+    and preg_match('/[0-9]/', $password)
+ ) {
+    return true;
+ }
+ return false;
+}
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $password = $_POST['password'];
+    $valid = is_password($password);
+    $message = $valid ? 'Password ir valid.' : 'Password not strong enaugh';
+}
+?>
+<?= $message ?>
+<form action="password-check.php" method="post">
+    Password: <input type="password" name="password">
+    <input type="submit" value="Save">
+</form>
+
+<?php 
+
+include __DIR__ . '/includes/footer.html.php';
